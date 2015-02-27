6e8f780cbc1231ffdc9139be2db2c0710651348e
MarkJosephSmith <markjosephsmith@gmail.com>
2015-02-26 19:13:32 -0700

    Upload before I mess anything up
    about to upload Steven's movable.

---
+++ KittyQuest/Assets/ClimbCollide.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	/*back to collision climb tests
+    * 
+    **/ 
+	void OnControllerColliderHit(ControllerColliderHit col)
+    {
+		Rigidbody body = col.collider.attachedRigidbody;
+
+		if (body == null || body.isKinematic) 
+		{
+			return;
+		}
+
+        if (body.gameObject.CompareTag("Climbable"))
+        {
+            Debug.Log("Colliding");
+            //check if we are currently climbing and call the appropriate function
+
+            //Climbing();
+
+        }
+    }
+	/*
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Climbable"))
+        {
+            Debug.Log("Collided, climbing is " + isClimbing);
+            //check if we are currently climbing and call the appropriate function
+            //StopClimbing();
+
+        }
+    }*/
+}
