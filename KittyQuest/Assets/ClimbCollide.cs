801267500985d25e4907fd02e6b52a553633f76f
MarkJosephSmith <markjosephsmith@gmail.com>
2015-03-01 22:43:51 -0700

    Setup for collision climb
    Setup for building collision climb from my home PC.  Add the stuff from
    the laptop and finish it out tomorrow.

--- KittyQuest/Assets/ClimbCollide.cs
+++ KittyQuest/Assets/ClimbCollide.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class ClimbCollide : MonoBehaviour {
 
+    bool isClimbing = false;  //is the character currently climbing
+    bool canClimb = false;  //is the character touching a climbable object
+    int grip = 100;  //how strong is your grip
+    bool onTop = false; //has the character reached the top of a climable object
+    bool touchedGround = false;  //has the character touched the ground since they started climbing
+
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +21,17 @@ public class ClimbCollide : MonoBehaviour {
 	
 	}
 
+    /*LateUpdate is used to turn off climbing.  This is done so
+     * that the character only keeps climbing if they collide with the
+     * climbable object again in the next frame.
+     */
+    void LateUpdate()
+    {
+        canClimb = false;
+    }
 
-	/*back to collision climb tests
+
+	/*collision climb tests
     * 
     **/ 
 	void OnControllerColliderHit(ControllerColliderHit col)
@@ -29,21 +46,32 @@ public class ClimbCollide : MonoBehaviour {
         if (body.gameObject.CompareTag("Climbable"))
         {
             Debug.Log("Colliding");
+
+
+
+            //check to see if we are at a correct 
+
             //check if we are currently climbing and call the appropriate function
 
             //Climbing();
 
         }
     }
-	/*
-    void OnCollisionExit(Collision col)
-    {
-        if (col.gameObject.CompareTag("Climbable"))
-        {
-            Debug.Log("Collided, climbing is " + isClimbing);
-            //check if we are currently climbing and call the appropriate function
-            //StopClimbing();
 
-        }
-    }*/
+    /*Function that does the needed checks and sets all the 
+     * needed variables so that the update function can begin climbing
+     * 
+     */
+    void Climbing()
+    {
+        
+    }
+
+    /*Function that sets all the needed variables so that the update function stops climbing
+     * 
+     */
+    void Climbing()
+    {
+
+    }
 }
