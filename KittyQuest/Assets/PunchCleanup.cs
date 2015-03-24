787fda42e26cd906f496a195489cfe2961802be8
MarkJosephSmith <markjosephsmith@gmail.com>
2015-03-24 17:44:56 -0600

    This should resolve the issues

---
+++ KittyQuest/Assets/PunchCleanup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchCleanup : MonoBehaviour {
+
+    bool punchDestroy = false;
+    int framesTraveled = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        framesTraveled += 1;
+        if (framesTraveled > 5)
+        {
+            punchDestroy = true;
+        }
+	}
+
+    void LateUpdate ()
+    {
+        if (punchDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        punchDestroy = true;
+    }
+}
