9bd7b630d264d56bcb91d80dac1debe432eb78d3
lokil88 <schmidt.b.steven@gmail.com>
2015-04-07 21:02:42 -0600

    Main Menu Added
    Added the Main Menu and Sub Menus as a separate scene.

---
+++ KittyQuest/Assets/Scripts/Tester.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tester : MonoBehaviour {
+
+	public bool isQuit;	
+	public bool isPlay;
+	public bool isControl;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnMouseUpAsButton ();
+	}
+
+	void OnMouseEnter (){
+
+	}
+
+	void OnMouseUpAsButton(){
+		if (isQuit) {
+			Application.Quit();
+		}
+		else if(isPlay){
+			Application.LoadLevel(1);
+		}
+		else if(isControl){
+
+		}
+
+	}
+	void OnMouseOver(){
+
+	}
+
+}
