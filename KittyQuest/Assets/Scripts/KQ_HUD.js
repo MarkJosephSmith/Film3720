bc1d1ec25fb72dd1f844f83ad38ac0a9ae7302dc
pdotedot <u0721449@1110mac-11.lib.utah.edu>
2015-04-07 19:46:27 -0600

    Patrick Update
    + Mini Map
    + HUD Life Counter
    + Scene

---
+++ KittyQuest/Assets/Scripts/KQ_HUD.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+import System.IO;
+
+public var Lives: int = 9;
+public var Objectives;
+
+private var lifeCounter: GUIText;
+
+function OnGUI () 
+{
+ 	GUI.Label (Rect (10, 10, 100, 20), String.Concat(Lives.ToString()," Lives"));
+}
