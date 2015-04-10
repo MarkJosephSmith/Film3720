c77eefe1365fd521f1395ff790cf79adc8a5c07c
lokil88 <u0947012@1110mac-31.lib.utah.edu>
2015-04-09 19:39:15 -0600

    Main Menu and Font

--- KittyQuest/Assets/MenuManager.cs
+++ KittyQuest/Assets/MenuManager.cs
@@ -10,7 +10,9 @@ public class MenuManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	 	if(Input.GetAxis("Vertical") > 0.01f){
+			Debug.Log("kjgfjhgf");
+		}
 	}
 
 	public void LoadDay(){
