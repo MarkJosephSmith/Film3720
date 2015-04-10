06181d845951b753dc7b9ca8010cacd1184e0b8a
pdotedot <u0721449@1110mac-11.lib.utah.edu>
2015-04-09 20:05:28 -0600

    MINI MAP Update
    Automatically switches between layers.

---
+++ KittyQuest/Assets/Scripts/KQ_Mini_Map.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+import System.IO;
+
+public var player: GameObject;
+public var floor: GameObject;
+
+function Update () 
+{
+	var mask: int;
+	var cullingMask: int = gameObject.GetComponent.<Camera>().cullingMask;
+
+    // Debug.Log("Mask = " + cullingMask.ToString());
+	if (player.transform.position.y > floor.transform.position.y)
+	{
+		mask = 1 << 8;
+		mask = ~mask;
+		cullingMask = cullingMask & mask;
+		mask = 1 << 9;
+		cullingMask = cullingMask | mask;
+	}
+	else
+	{
+		mask = 1 << 8;
+		cullingMask = cullingMask | mask;
+		mask = 1 << 9;
+		mask = ~mask;
+		cullingMask = cullingMask & mask;
+	}
+	//mm_camera
+
+	gameObject.GetComponent.<Camera>().cullingMask = cullingMask;
+}
