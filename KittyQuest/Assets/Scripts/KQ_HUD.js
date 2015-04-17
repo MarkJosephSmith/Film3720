cacb207d3bf7f90894fdacb514ef28a4bfe805d7
pdotedot <u0721449@1110mac-33.lib.utah.edu>
2015-04-16 20:03:54 -0600

    HUD ELEMENT UPDATE

--- KittyQuest/Assets/Scripts/KQ_HUD.js
+++ KittyQuest/Assets/Scripts/KQ_HUD.js
@@ -2,12 +2,47 @@
 
 import System.IO;
 
+// For Displaying and Counting Lives
 public var Lives: int = 9;
-public var Objectives;
-
+public var kitty: Texture;
 private var lifeCounter: GUIText;
+
+// For Displaying and Creating Objectives
+public var objectives : GameObject[] = new GameObject[5];
+private var indexObj: int = 0;
 
 function OnGUI () 
 {
- 	GUI.Label (Rect (10, 10, 100, 20), String.Concat(Lives.ToString()," Lives"));
+	if (kitty != null)
+	{
+		for (var i = 0; i < Lives; i++)
+		{
+			GUI.DrawTexture(Rect(10 * i + 50 * i, 10, 50, 50), kitty, ScaleMode.ScaleToFit, false, 0);
+		}
+ 	}
+ 	
+  	var centeredStyle = GUI.skin.GetStyle("Label");
+    centeredStyle.alignment = TextAnchor.UpperCenter;	
+ 	if (indexObj >= objectives.Length)
+ 	{
+
+ 		GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Take a nap in a sunspot.", centeredStyle);
+ 	} 
+ 	else
+ 	{
+ 		var objective: GameObject = objectives[indexObj];
+ 		if (objective.transform.tag == "Moveable")
+ 		{
+ 			GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50), ("Knock over the " + objective.transform.name), centeredStyle);
+ 		}
+ 		else if (objective.transform.tag == "Destination")
+ 		{
+ 			Debug.Log("HELLO!");
+ 			GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50), ("Go to the " + objective.transform.name), centeredStyle);
+ 		}
+ 		else if (objective.transform.tag == "Climbable")
+ 		{
+ 		
+ 		}
+ 	}
 }
--- KittyQuest/Assets/Scripts/KQ_Mini_Map.js
+++ KittyQuest/Assets/Scripts/KQ_Mini_Map.js
@@ -4,14 +4,23 @@ import System.IO;
 public var player: GameObject;
 public var floor: GameObject;
 
-function Update () 
+function Start()
+{
+	InvokeRepeating("CheckFloor",0.0,1.0);
+}
+
+function CheckFloor() 
 {
 	var mask: int;
 	var cullingMask: int = gameObject.GetComponent.<Camera>().cullingMask;
 
+	// FLOOR 1 is LAYER 8
+	// FLOOR 2 is LAYER 9
+
     // Debug.Log("Mask = " + cullingMask.ToString());
 	if (player.transform.position.y > floor.transform.position.y)
 	{
+		//Debug.Log("Player at: " + player.transform.position.y + " Floor at: " + floor.transform.position.y);
 		mask = 1 << 8;
 		mask = ~mask;
 		cullingMask = cullingMask & mask;
@@ -20,13 +29,11 @@ function Update ()
 	}
 	else
 	{
-		mask = 1 << 8;
-		cullingMask = cullingMask | mask;
 		mask = 1 << 9;
 		mask = ~mask;
 		cullingMask = cullingMask & mask;
+		mask = 1 << 8;
+		cullingMask = cullingMask | mask;
 	}
-	//mm_camera
-
 	gameObject.GetComponent.<Camera>().cullingMask = cullingMask;
 }
