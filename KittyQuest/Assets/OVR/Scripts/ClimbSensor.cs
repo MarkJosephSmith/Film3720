5823af2a8a1a7af24ac8de35576571a0695762b5
MarkJosephSmith <markjosephsmith@gmail.com>
2015-04-27 23:49:57 -0600

    Another approach to climbing
    Works a little better, maybe.

---
+++ KittyQuest/Assets/OVR/Scripts/ClimbSensor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbSensor : MonoBehaviour {
+
+	//public bool isClimbing;
+
+	// Use this for initialization
+	void Start () {
+		//isClimbing = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+
+	}
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("test");
+		Rigidbody body = col.GetComponent<Collider>().attachedRigidbody;
+		
+		if (body == null || body.isKinematic) 
+		{
+			return;
+		}
+
+		if (body.gameObject.CompareTag ("Climbable")) 
+		{
+			Debug.Log ("start colliding");
+			this.transform.parent.gameObject.GetComponent<ClimbCollide>().isClimbing = true;
+
+		}
+	}
+
+
+	void OnTriggerExit(Collider col)
+	{
+		Rigidbody body = col.GetComponent<Collider>().attachedRigidbody;
+		
+		//if (body == null || body.isKinematic) 
+		//{
+		//	return;
+		//}
+		
+		if (body.gameObject.CompareTag ("Climbable")) 
+		{
+			Debug.Log ("stopped colliding");
+			
+			this.transform.parent.gameObject.GetComponent<ClimbCollide>().isClimbing = false;
+		}
+	}
+}
--- KittyQuest/Assets/Scripts/ClimbCollide.cs
+++ KittyQuest/Assets/Scripts/ClimbCollide.cs
@@ -9,10 +9,11 @@ using System.Collections;
 
 public class ClimbCollide : MonoBehaviour {
 
-    bool isClimbing = false;  //is the character currently climbing
+    //bool climbing = false;  //is the character currently climbing
     bool wasClimbing = false;  //is the character touching a climbable object
+	public bool isClimbing = false;
     int grip = 100;  //how strong is your grip
-    float topOfClimbable = 100000; //stores the top of a climable object
+    //float topOfClimbable = 100000; //stores the top of a climable object
 
 
 
@@ -24,46 +25,92 @@ public class ClimbCollide : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (isClimbing)
-        {
-            //first call 
-            //Debug.Log("first call" + isClimbing);
+		//ClimbSensor tempSensor = this.transform.FindChild ("ClimbSensor");
+		//bool touching = tempSensor.isClimbing;
+		
 
-            //adjust movement, gravity and velocity
-            StartClimbing();
+		//if we weren't climbing before but are now, set the climbing variables
+		if (isClimbing && !wasClimbing) 
+		{
+
+				//adjust movement, gravity and velocity
+				StartClimbing ();
+		}
+
+		//if we were climbing before but aren't now, undo the climbing valiables
+		//if we weren't climbing before but are now, set the climbing variables
+		if (!(isClimbing) && wasClimbing) {
+			
+			//adjust movement, gravity and velocity
+			StopClimbing ();
+		} else if (wasClimbing && this.GetComponent<CharacterController> ().isGrounded) 
+		{
+			//
+			StopClimbing();
+		}
+
+		//check to see if we are climbing and adjust the 
+		if (isClimbing) 
+		{
+				//check to see if we are on the ground or not and set button input appropriatly
+				// positive "vertical" moves you up and resets your grip
+				if (Input.GetAxis ("Vertical") > 0.01f) {
+						grip = 100;
+						//this.gameObject.transform.Translate(0.0f,0.2f,0.0f,Space.World);
+						this.gameObject.GetComponent<CharacterController> ().Move ((new Vector3 (0.0f, 0.2f, 0.0f)));
+
+				}
+
+				// negative "vertical" moves you down if you're not grounded and resets your grip
+				if (this.GetComponent<CharacterController> ().isGrounded) {
+						//if grounded nothing changes
+				}
+				//if we aren't grounded change to climb down.
+				else if (Input.GetAxis ("Vertical") < -0.01f) {
+						grip = 100;
+						//this.gameObject.transform.Translate(0.0f, -0.2f, 0.0f, Space.World);
+						this.gameObject.GetComponent<CharacterController> ().Move ((new Vector3 (0.0f, -0.2f, 0.0f)));
 
 
-            //check to see if we are on the ground or not and set button input appropriatly
-            // "w" moves you up and resets your grip
-            if (Input.GetAxis("Vertical") > 0.01f)
-            {
-                System.Console.WriteLine(isClimbing);
-                grip = 100;
-                //this.gameObject.transform.Translate(0.0f,0.2f,0.0f,Space.World);
-                this.gameObject.GetComponent<CharacterController>().Move(new Vector3(0.0f, 0.2f, 0.0f));
+				}
+		}
 
+    }
 
-            }
+	void StartClimbing()
+	{
+		//disable current movement and gravity
+		//stop any current motion, just setting velocity wasn't stoping the character, this is quick and dirty fix.
+		this.gameObject.GetComponent<CharacterMotor>().movement.maxForwardSpeed = 0.0f;
+		this.gameObject.GetComponent<CharacterMotor>().movement.maxBackwardsSpeed = 0.0f;
+		this.gameObject.GetComponent<CharacterMotor>().movement.maxSidewaysSpeed = 0.0f;
+		this.gameObject.GetComponent<CharacterMotor>().movement.gravity = 0.0f;  //we will handle gravity in the update function because it's a pain in the ass.
+		this.gameObject.GetComponent<CharacterMotor>().movement.velocity = Vector3.zero;
+		this.gameObject.GetComponent<CharacterMotor>().movement.frameVelocity = Vector3.zero;
+		
+	}
+	
+	/*Function that sets all the needed variables so that the update function stops climbing
+     * 
+     */
+	void StopClimbing()
+	{
+		//isClimbing = false;
+		
+		//restore all the the max variables to their original
+		this.gameObject.GetComponent<CharacterMotor>().movement.maxForwardSpeed = 10.0f;
+		this.gameObject.GetComponent<CharacterMotor>().movement.maxBackwardsSpeed = 10.0f;
+		this.gameObject.GetComponent<CharacterMotor>().movement.maxSidewaysSpeed = 10.0f;
+		this.gameObject.GetComponent<CharacterMotor>().movement.gravity = 10.0f; //we will handle gravity in the update function because it's a pain in the ass.
+	}
 
-            // "s" moves you down if you're not grounded and resets your grip
-            if (this.GetComponent<CharacterController>().isGrounded)
-            {
-                //if so nothing changes
-            }
-            //if we aren't grounded change the s key to climb down.
-			else if (Input.GetAxis("Vertical") < -0.01f) 
-            {
-                grip = 100;
-                //this.gameObject.transform.Translate(0.0f, -0.2f, 0.0f, Space.World);
-                this.gameObject.GetComponent<CharacterController>().Move(new Vector3(0.0f, -0.2f, 0.0f));
-
-
-            }
-
-        }
+	void LateUpdate()
+	{
+		wasClimbing = isClimbing;
+	}
 
 	
-	}
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////Old code I may need to reference.
 
     /*LateUpdate is used to turn off climbing.  This is done so
      * that the character only keeps climbing if they collide with the
@@ -71,8 +118,9 @@ public class ClimbCollide : MonoBehaviour {
      * 
      * If they were and no longer are this will trigger a return to standard movement
      */
-    void LateUpdate()
-    {
+    //void LateUpdate()
+    //{
+		/*
         //Debug.Log("feet at " + (this.gameObject.GetComponent<CharacterController>().center.y - (this.gameObject.GetComponent<CharacterController>().height / 2)) + "height at " + topOfClimbable);
         //Debug.Log("feet at " + (this.gameObject.GetComponent<CharacterController>().transform.position.y - (this.gameObject.GetComponent<CharacterController>().height / .5)) + "height at " + topOfClimbable);
         //Debug.Log("feet at " + (this.gameObject.GetComponent<CharacterController>().transform.position.y) + "height at " + topOfClimbable);
@@ -91,15 +139,16 @@ public class ClimbCollide : MonoBehaviour {
             Debug.Log("reached top");
             StopClimbing();
         }
+        */
 
         
         
-    }
+    //}
 
 
 	/*collision climb tests
     * 
-    **/ 
+    *
 	void OnControllerColliderHit(ControllerColliderHit col)
     {
 		Rigidbody body = col.collider.attachedRigidbody;
@@ -127,36 +176,11 @@ public class ClimbCollide : MonoBehaviour {
 
 
         }
-    }
+    }*/ 
 
     /*Function that does the needed checks and sets all the 
      * needed variables so that the update function can begin climbing
      * 
      */
-    void StartClimbing()
-    {
-        //disable current movement and gravity
-        //stop any current motion, just setting velocity wasn't stoping the character, this is quick and dirty fix.
-        this.gameObject.GetComponent<CharacterMotor>().movement.maxForwardSpeed = 0.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.maxBackwardsSpeed = 0.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.maxSidewaysSpeed = 0.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.gravity = 0.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.velocity = Vector3.zero;
-        this.gameObject.GetComponent<CharacterMotor>().movement.frameVelocity = Vector3.zero;
-        
-    }
 
-    /*Function that sets all the needed variables so that the update function stops climbing
-     * 
-     */
-    void StopClimbing()
-    {
-        isClimbing = false;
-
-        //restore all the the max variables to their original
-        this.gameObject.GetComponent<CharacterMotor>().movement.maxForwardSpeed = 10.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.maxBackwardsSpeed = 10.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.maxSidewaysSpeed = 10.0f;
-        this.gameObject.GetComponent<CharacterMotor>().movement.gravity = 10.0f;
-    }
 }
