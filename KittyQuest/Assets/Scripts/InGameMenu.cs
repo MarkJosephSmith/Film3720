64ccc2f2acaf482320fe369b8dda2194b2a3111a
lokil88 <schmidt.b.steven@gmail.com>
2015-04-28 18:40:42 -0600

    Ingame Menu
    Created the Ingame Menu.

---
+++ KittyQuest/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour 
+{
+	public Canvas IngameMenu;
+	// Use this for initialization
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		IngameMenu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.Escape))
+		{
+			if(IngameMenu.enabled = false)
+			{
+				IngameMenu.enabled = true;
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+			if(IngameMenu.enabled = true)
+			{
+				IngameMenu.enabled = false;
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+		}
+	}
+}
