e0da1990171d34dbc4d95c2dee9ec479299479db
MarkJosephSmith <markjosephsmith@gmail.com>
2015-05-04 21:23:52 -0600

    Added cat player prefab and bottle to day 3

---
+++ KittyQuest/Assets/Scripts/Found.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Found : MonoBehaviour {
+
+	bool isFound = false;
+	//bool isPunched = false;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+	//if the player enters the trigger box, turn off the particles and mark it as found
+	void OnTriggerEnter(Collider col)
+	{
+		//when the player enters the trigger
+		if (col.tag == "Player") 
+		{
+			//check to see if it's the first time
+			if (isFound == false)
+			{
+
+
+				//if so shut off the particles
+				this.gameObject.transform.GetComponentInChildren<ParticleSystem>().enableEmission = false;
+
+				//set it to true
+				isFound = true;
+
+				//incriment the objective count
+			
+			} 
+
+		}
+
+		return;
+	}
+}
