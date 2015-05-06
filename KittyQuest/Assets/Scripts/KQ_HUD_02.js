43e431aed78666cdacb10b95938c18d8b8a701cf
pdotedot <pdotedot@gmail.com>
2015-05-06 16:34:23 -0600

    DAY 2 Objectives

--- KittyQuest/Assets/Scripts/KQ_HUD_02.js
+++ KittyQuest/Assets/Scripts/KQ_HUD_02.js
@@ -1,5 +1,5 @@
 ﻿#pragma strict
-// DAY 01 KITTY QUEST
+// DAY 02 KITTY QUEST
 // For Displaying and Counting Lives
 import System.IO;
 
@@ -7,6 +7,7 @@ public var Lives: int = 9;
 public var myFont: Font;
 private var lifeCounter: GUIText;
 private var roomVisits: int = 0;
+private var stairItems: int = 0;
 private var lifeText = ["N","I","N","E ","L","I","V","E","S"];
 
 // For Displaying and Creating Objectives
@@ -20,22 +21,21 @@ function OnGUI ()
  	switch (indexObjective)
  	{
  	case 0: // EXPLORE THE HOUSE
-		 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Explore the house.", centeredStyle);
+		 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"The 2nd Floor is accessible, go explore Upstairs.", centeredStyle);
  	break;
  	case 1: // Find Ball
- 	 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Find the Ball in the Living Room.", centeredStyle);
-
+ 		 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Knock boxes down the Stairs.", centeredStyle);
  	break;
  	case 2: // ball to kitchen
- 	 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Click to bat the Ball into the Kitchen.", centeredStyle);
+ 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Get back to the 1st Floor WITHOUT using the Stairs.", centeredStyle);
  	break;
  	case 3: // ball to entryway
- 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Bat the Ball back to the Front Door.", centeredStyle);
+ 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Take an inconspicuous nap in the Living Room Sunspot.", centeredStyle);
  	
  	break;
- 	case 4: // find a vantage point
- 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Find a sunspot in the Living Room to nap in.", centeredStyle);
- 	break;
+// 	case 4: // find a vantage point
+// 	 	 GUI.Label(Rect(Screen.width/4, Screen.height * 0.8, Screen.width/2, 50),"Find a sunspot in the Living Room to nap in.", centeredStyle);
+// 	break;
  	default:
  	break;
  	}
@@ -57,27 +57,28 @@ function OnGUI ()
 		 switch (indexObjective)
  		{
  		case 0: // EXPLORE THE HOUSE
-			 if(objective.Contains("ROOM"))
+			 if(objective.Contains("ROOM2"))
 			 	roomVisits++;
-			 if (roomVisits >= 4)
+			 if (roomVisits >= 3)
 			 	indexObjective++;
  		break;
- 		case 1: // Find Ball
-			if("BALL".Equals(objective))
-			 	indexObjective++;
- 		break;
+ 		case 1: // Find way downstairs
+			if("STAIRWAY".Equals(objective))
+			 	stairItems++;
+			 if (stairItems >= 3)
+			 	indexObjective++; 		break;
  		case 2: // ball to kitchen
- 			if("KITCHEN".Equals(objective))
+ 			if("BALCONY".Equals(objective))
 			 	indexObjective++;
  		break;
  		case 3: // ball to stairs
- 	 		if("FRONTDOOR".Equals(objective))
+ 	 		if("SUNSPOT".Equals(objective))
 			 	indexObjective++;
  		break;
-		case 4:
-		 	 if("SUNSPOT".Equals(objective))
-			 	indexObjective++;
-		break;
+//		case 4:
+//		 	 if("SUNSPOT".Equals(objective))
+//			 	indexObjective++;
+//		break;
  		default: // nap in the sunspot
  		break;
  		}
